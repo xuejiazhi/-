c1a52be3f673560d8b4f90faf409541dab3fd4da
xjz <xuejiazhi@gmail.com>
2023-11-10 17:39:44 +0800

    测试

---
+++ res/js/modules/index.js
@@ -0,0 +1,7 @@
+layui.define(['layer','form','laydate'],function (exports){
+    var layer = layui.layer
+        ,form = layui.form;
+    layer.msg('Hello World');
+    laydate.render({});
+    exports('index',{});
+});
